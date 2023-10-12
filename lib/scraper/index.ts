32803a7647bc30845e34cf3ace9097d5e48817b1
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-12 13:52:53 -0400

    create util function for scraping required data

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
 
+import { extractPrice } from '../utils';
+
 export async function scrapeAmazonProduct(url: string) {
   if(!url) return;
 
@@ -26,7 +28,19 @@ export async function scrapeAmazonProduct(url: string) {
     
     // Extract product title
     const title = $('#productTitle').text().trim();
-    console.log('title', title)
+    const currentPrice = extractPrice(
+      $('.priceToPay span.a-price-whole'),
+      $('a.size.base.a-color-price'),
+      $('.a-button-selected .a-color-base')
+    );
+
+    const originalPrice = extractPrice(
+      $('#priceblock_ourprice'),
+      $('.a-price.a-text-price span.a-offscreen'),
+    )
+
+    console.log('title', title);
+    console.log('currentPrice', currentPrice);
   } catch(error: any) {
     throw new Error(`Failed to scrape product: ${error.message}`)
   }
