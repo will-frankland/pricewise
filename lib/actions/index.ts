d66f5c81717fcfd84f2f7aa36d9586b66bf9a85f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-16 10:53:46 -0400

    create Product model

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -11,6 +11,10 @@ export async function scrapeAndStoreProduct(productUrl: string) {
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
     
     if(!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    // const existingProduct = await Product
     
   } catch(error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
