e8d3a8360fcf3915526c3c25aad8d6b72e874d01
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-16 11:00:01 -0400

    find new product in DB or create if doesnt exist

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,22 +1,47 @@
-"use server"
+"use server";
 
+import { revalidatePath } from "next/cache";
 import { scrapeAmazonProduct } from "../scraper";
 import { connectToDB } from "../mongoose";
+import Product from "../models/product.model";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
-  if(!productUrl) return;
+  if (!productUrl) return;
 
   try {
     connectToDB();
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
-    
-    if(!scrapedProduct) return;
+
+    if (!scrapedProduct) return;
 
     let product = scrapedProduct;
 
-    // const existingProduct = await Product
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
     
-  } catch(error: any) {
-    throw new Error(`Failed to create/update product: ${error.message}`)
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`);
   }
-};
+}
