98a306e8651c7dfe0be56d456894c52ca40c9355
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-18 17:30:19 -0400

    begin styling productCard component and passing in scraped data to display

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,6 @@
-import { Product } from "@/types"
+import { Product } from "@/types";
+import Link from "next/link";
+import Image from "next/image";
 
 interface Props {
   product: Product;
@@ -6,10 +8,30 @@ interface Props {
 
 const ProductCard = ({ product }: Props) => {
   return (
-    <div>
-      Product Card
-    </div>
-  )
-}
+    <Link href={`/products/${product._id}`} className="product-card">
+      <div className="product-card_img-container">
+        <Image
+          src={product.image}
+          alt={product.title}
+          width={200}
+          height={200}
+          className="product-card_img"
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <h3 className="product-title">
+          {product.title}
+        </h3>
+        <div className="flex justify-between">
+          <p className="text-black opacity-50 text-lg capitalize">{product.category}</p>
+          <p className="text-black text-lg font-semibold">
+            <span >{product?.currency}</span>
+            <span>{product?.currentPrice}</span>
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
