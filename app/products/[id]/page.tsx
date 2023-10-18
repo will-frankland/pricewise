723bccd0e604a61d4ca260a82ed770d862a135d3
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-18 17:47:39 -0400

    add review count and save button to product details, set Props from Product

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,13 +1,68 @@
+import { getProductById } from "@/lib/actions";
+import { redirect } from "next/navigation";
+import { Product } from "@/types";
+import Image from "next/image";
+import Link from "next/link";
+
 type Props = {
-  params: { id: string }
-}
+  params: { id: string };
+};
 
-const ProductDetails = ({ params: { id } }: Props) => {
+const ProductDetails = async ({ params: { id } }: Props) => {
+  const product: Product = await getProductById(id);
+
+  if (!product) redirect("/");
+
   return (
-    <div>
-      {id}
+    <div className="product-container">
+      <div className="flex gap-28 xl:flex-row flex-col">
+        <div className="product-image">
+          <Image
+            src={product.image}
+            alt={product.title}
+            width={580}
+            height={400}
+            className="mx-auto"
+          />
+        </div>
+        <div className="flex-1 flex flex-col">
+          <div className="flex justify-between items-0start gap-5 flex-wrap pb-6">
+            <div className="flex flex-col gap-3">
+              <p className="text-[28px text-secondary] font-semibold">
+                {product.title}
+              </p>
+              <Link
+                href={product.url}
+                target="_blank"
+                className="text-base text-black opacity-50"
+              >
+                Visit Product
+              </Link>
+            </div>
+            <div className="flex items-center gap-3">
+              <div className="product-hearts">
+                <Image
+                  src={'/assets/icons/red-heart.svg'}
+                  alt="heart"
+                  width={20}
+                  height={20}
+                />
+                <p className="text-base font-semibold text-[#D46F77]">{product.reviewsCount}</p>
+              </div>
+              <div className="p-2 bg-white-200 rounded-10">
+                <Image
+                  src={"/assets/icons/bookmark.svg"}
+                  alt="bookmark"
+                  width={20}
+                  height={20}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductDetails
+export default ProductDetails;
