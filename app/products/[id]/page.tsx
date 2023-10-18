992fb4a8fe5d7c8a5ee3ba5466e93f183b476f17
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-18 18:09:05 -0400

    remove dynamic border due to tailwind issue

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -44,12 +44,14 @@ const ProductDetails = async ({ params: { id } }: Props) => {
             <div className="flex items-center gap-3">
               <div className="product-hearts">
                 <Image
-                  src={'/assets/icons/red-heart.svg'}
+                  src={"/assets/icons/red-heart.svg"}
                   alt="heart"
                   width={20}
                   height={20}
                 />
-                <p className="text-base font-semibold text-[#D46F77]">{product.reviewsCount}</p>
+                <p className="text-base font-semibold text-[#D46F77]">
+                  {product.reviewsCount}
+                </p>
               </div>
               <div className="p-2 bg-white-200 rounded-10">
                 <Image
@@ -71,34 +73,41 @@ const ProductDetails = async ({ params: { id } }: Props) => {
           </div>
           <div className="product-info">
             <div className="flex flex-col gap-2">
-              <p className="text-[34px] text-secondary font-bold">{product.currency} {formatNumber(product.currentPrice)}</p>
-              <p className="text-[21px] text-black opacity-50 line-through">{product.currency} {formatNumber(product.originalPrice)}</p>
+              <p className="text-[34px] text-secondary font-bold">
+                {product.currency} {formatNumber(product.currentPrice)}
+              </p>
+              <p className="text-[21px] text-black opacity-50 line-through">
+                {product.currency} {formatNumber(product.originalPrice)}
+              </p>
             </div>
             <div className="flex flex-col gap-4">
               <div className="flex gap-3">
                 <div className="product-stars">
                   <Image
-                    src={'/assets/icons/star.svg'}
+                    src={"/assets/icons/star.svg"}
                     alt="star"
                     width={16}
                     height={16}
                   />
-                  <p className="text-sm text-primary-orange font-semibold">{product.stars || '25'}</p>
+                  <p className="text-sm text-primary-orange font-semibold">
+                    {product.stars || "25"}
+                  </p>
                 </div>
                 <div className="product-reviews">
                   <Image
-                    src={'/assets/icons/comment.svg'}
+                    src={"/assets/icons/comment.svg"}
                     alt="comments"
                     width={16}
                     height={16}
                   />
-                  <p className="text-sm text-secondary font-semibold">{product.reviewsCount}</p>
+                  <p className="text-sm text-secondary font-semibold">
+                    {product.reviewsCount}
+                  </p>
                 </div>
               </div>
               <p className="text-sm text-black opacity-50">
-                <span className="text-primary-green font-smeibold">
-                  93%
-                </span> of buyers have recommended this.
+                <span className="text-primary-green font-smeibold">93%</span> of
+                buyers have recommended this.
               </p>
             </div>
           </div>
@@ -107,8 +116,30 @@ const ProductDetails = async ({ params: { id } }: Props) => {
               <PriceInfoCard
                 title="Current Price"
                 iconSrc="/assets/icons/price-tag.svg"
-                value={`${product.currency} ${formatNumber(product.currentPrice)}`}
-                borderColor="#B6DBFF"
+                value={`${product.currency} ${formatNumber(
+                  product.currentPrice
+                )}`}
+              />
+              <PriceInfoCard
+                title="Average Price"
+                iconSrc="/assets/icons/chart.svg"
+                value={`${product.currency} ${formatNumber(
+                  product.averagePrice
+                )}`}
+              />
+              <PriceInfoCard
+                title="Highest Price"
+                iconSrc="/assets/icons/arrow-up.svg"
+                value={`${product.currency} ${formatNumber(
+                  product.highestPrice
+                )}`}
+              />
+              <PriceInfoCard
+                title="Lowest Price"
+                iconSrc="/assets/icons/arrow-down.svg"
+                value={`${product.currency} ${formatNumber(
+                  product.lowestPrice
+                )}`}
               />
             </div>
           </div>
