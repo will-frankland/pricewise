1833d19a6ff0bd100ff332ca75836e5d912ca551
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-20 10:54:41 -0400

    create getSimilarProducts function and add description and buy now btn to product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -143,8 +143,34 @@ const ProductDetails = async ({ params: { id } }: Props) => {
               />
             </div>
           </div>
+          Modal
         </div>
       </div>
+      <div className="flex flex-col gap-16 border-2 border-red-500">
+        <div className="flex flex-col gap-5">
+          <h3 className="text-2xl test-secondary font-semibold">
+            Product Description
+          </h3>
+          <div className="flex flex-col gap-4">
+            {product?.description?.split('\n')}
+          </div>
+        </div>
+        <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]">
+          <Image
+            src='/assets/icons/bag.svg'
+            alt="check"
+            width={22}
+            height={22}
+          />
+          <Link
+            href="/"
+            className="text-base text-white"
+          >
+            Buy Now
+          </Link>
+        </button>
+      </div>
+      
     </div>
   );
 };
