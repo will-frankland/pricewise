e1b928cc116a8e6770064f920b7f40c49a4c0480
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-20 11:29:57 -0400

    add content to modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -59,8 +59,40 @@ const Modal = () => {
                         height={28}
                       />
                     </div>
+                    <Image
+                      src="/assets/icons/x-close.svg"
+                      alt="close"
+                      width={24}
+                      height={24}
+                      className="cursor-pointer"
+                      onClick={closeModal}
+                    />
                   </div>
+                  <h4>
+                    Stay updated with product pricing alerts in your inbox!
+                  </h4>
+                  <p>Never miss a bargain again with our timely alerts!</p>
                 </div>
+                <form className="flex flex-col mt-5">
+                  <label htmlFor="email" className="text-sm font-medium text-gray-700">
+                    Email Address
+                  </label>
+                  <div className="dialog-input_container">
+                    <Image
+                      src="/assets/icons/mail.svg"
+                      alt="mail"
+                      width={18}
+                      height={18}
+                    />
+                    <input 
+                      required
+                      type="email"
+                      id="email"
+                      placeholder="Enter your email address"
+                      className="dialog-input"
+                    />
+                  </div>
+                </form>
               </div>
             </Transition.Child>
           </div>
