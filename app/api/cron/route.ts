19b81be1bc9c2e315ff6ac87ad6c6d65cd64ad94
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-20 13:22:25 -0400

    implement start of cron function and update average to averagePrice in scraped to solve error'

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -1,5 +1,7 @@
-import { connectToDB } from "@/lib/mongoose"
+import { connectToDB } from "@/lib/mongoose";
 import Product from "@/lib/models/product.model";
+import { scrapeAmazonProduct } from "@/lib/scraper";
+import { getAveragePrice, getEmailNotifType, getHighestPrice, getLowestPrice } from "@/lib/utils";
 
 export async function GET() {
   try {
@@ -7,9 +9,37 @@ export async function GET() {
 
     const products = await Product.find({});
 
-    if(!products) throw new Error("No products found")
+    if (!products) throw new Error("No products found");
 
+    // Scrape latest product details and update DB
+    const updatedProducts = await Promise.all(
+      products.map(async (currentProduct) => {
+        const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
+        if (!scrapedProduct) throw new Error("No product found");
+
+        const updatedPriceHistory = [
+          ...currentProduct.priceHistory,
+          { price: scrapedProduct.currentPrice },
+        ];
+        const product = {
+          ...scrapedProduct,
+          priceHistory: updatedPriceHistory,
+          lowestPrice: getLowestPrice(updatedPriceHistory),
+          highestPrice: getHighestPrice(updatedPriceHistory),
+          averagePrice: getAveragePrice(updatedPriceHistory),
+        };
+
+        const updatedProduct = await Product.findOneAndUpdate(
+          { url: scrapedProduct.url },
+          product
+        );
+
+        // Check each products status and send email accordingly
+          const emailNotifType = getEmailNotifType(scrapedProduct, currentProduct)
+
+      })
+    );
   } catch (error: any) {
-    throw new Error(`Error in GET: ${error}`)
+    throw new Error(`Error in GET: ${error}`);
   }
 }
