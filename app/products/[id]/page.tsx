ac946d98353cc24081f52d80a7872e3ef5b5cf5a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-10-20 10:59:16 -0400

    add similar Products and code to product page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,11 @@
-import { getProductById } from "@/lib/actions";
+import { getProductById, getSimilarProducts } from "@/lib/actions";
 import { redirect } from "next/navigation";
 import { Product } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import { formatNumber } from "@/lib/utils";
 import PriceInfoCard from "@/components/PriceInfoCard";
+import ProductCard from "@/components/ProductCard";
 
 type Props = {
   params: { id: string };
@@ -14,6 +15,8 @@ const ProductDetails = async ({ params: { id } }: Props) => {
   const product: Product = await getProductById(id);
 
   if (!product) redirect("/");
+
+  const similarProducts = await getSimilarProducts(id);
 
   return (
     <div className="product-container">
@@ -146,31 +149,37 @@ const ProductDetails = async ({ params: { id } }: Props) => {
           Modal
         </div>
       </div>
-      <div className="flex flex-col gap-16 border-2 border-red-500">
+      <div className="flex flex-col gap-16">
         <div className="flex flex-col gap-5">
           <h3 className="text-2xl test-secondary font-semibold">
             Product Description
           </h3>
           <div className="flex flex-col gap-4">
-            {product?.description?.split('\n')}
+            {product?.description?.split("\n")}
           </div>
         </div>
         <button className="btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]">
           <Image
-            src='/assets/icons/bag.svg'
+            src="/assets/icons/bag.svg"
             alt="check"
             width={22}
             height={22}
           />
-          <Link
-            href="/"
-            className="text-base text-white"
-          >
+          <Link href="/" className="text-base text-white">
             Buy Now
           </Link>
         </button>
       </div>
-      
+      {similarProducts && similarProducts?.length > 0 && (
+        <div className="py-14 flex flex-col gap-2 w-full">
+          <p className="section-text">Similar Products</p>
+          <div className="flex flex-wrap gap-10 mt-7 w-full">
+            {similarProducts.map((product) => (
+              <ProductCard key={product._id} product={product} />
+            ))}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
